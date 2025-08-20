9ca9595b7b4c76e57728e03b4f026d699ea4ef74
Patrick Do <patrickdo343@gmail.com>
2025-08-19 21:01:28 -0500

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: resolve(__dirname),
+  root: resolve(import.meta.dirname),
   build: {
     outDir: 'dist'
   },
   server: {
     port: 3000
   },
-  publicDir: 'public'
 });
