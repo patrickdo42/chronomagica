68b2035b80ade5070ab9a7703f1d4caf2f9c0396
Patrick Do <patrickdo343@gmail.com>
2025-09-22 19:51:21 -0500

    mobile optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+<html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased`}
       >
